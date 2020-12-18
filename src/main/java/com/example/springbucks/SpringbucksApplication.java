759864c21856a140e1e3d1f624aaff5dfa6fd63d
c0n95hv <congshu@wo.cn>
2020-12-18 11:07:22 +0800

    Hello World!

--- src/main/java/com/example/springbucks/SpringbucksApplication.java
+++ src/main/java/com/example/springbucks/SpringbucksApplication.java
@@ -2,12 +2,19 @@ package com.example.springbucks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbucksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbucksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbucksApplication.class, args);
+    }
 
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
