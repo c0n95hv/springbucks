d5673e4b7a248293d7ccb6c555676432ee7112a7
c0n95hv <congshu@wo.cn>
2020-12-21 20:24:37 +0800

    druid-demo

--- src/main/java/com/example/springbucks/SpringbucksApplication.java
+++ src/main/java/com/example/springbucks/SpringbucksApplication.java
@@ -2,66 +2,27 @@ package com.example.springbucks;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        JdbcTemplateAutoConfiguration.class})
+@SpringBootApplication()
 @Slf4j
-public class SpringbucksApplication {
+public class SpringbucksApplication implements CommandLineRunner {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbucksApplication.class, args);
     }
 
-    @Bean
-    @ConfigurationProperties("foo.datasource")
-    public DataSourceProperties fooDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    public DataSource fooDataSource() {
-        DataSourceProperties dataSourceProperties = fooDataSourceProperties();
-        log.info("foo datasource: {}", dataSourceProperties.getUrl());
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean
-    @Resource
-    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
-        return new DataSourceTransactionManager(fooDataSource);
-    }
-
-    @Bean
-    @ConfigurationProperties("bar.datasource")
-    public DataSourceProperties barDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    public DataSource barDataSource() {
-        DataSourceProperties dataSourceProperties = barDataSourceProperties();
-        log.info("bar datasource: {}", dataSourceProperties.getUrl());
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean
-    @Resource
-    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
-        return new DataSourceTransactionManager(barDataSource);
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(dataSource.toString());
     }
 }
