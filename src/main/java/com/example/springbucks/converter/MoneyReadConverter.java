d5e467d1e0bf70fce0771c4a36b95f1125c5e15d
c0n95hv <congshu@wo.cn>
2021-03-05 10:59:14 +0800

    mongo-repository-demo

--- src/main/java/com/example/springbucks/converter/MoneyReadConverter.java
+++ src/main/java/com/example/springbucks/converter/MoneyReadConverter.java
@@ -4,7 +4,9 @@ import org.bson.Document;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MoneyReadConverter implements Converter<Document, Money> {
     @Override
     public Money convert(Document source) {
